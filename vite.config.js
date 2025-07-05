7380022380ee4a3d4f2cac4b4a06e1d6a0472758
sriram A <sriram.coder2004@gmail.com>
2025-07-05 11:07:20 +0530

    depoly  changes update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//     base: '/sonachala-userfrontend/', // ✅ IMPORTANT
+// })
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/sonachala-userfrontend/', // ✅ IMPORTANT
+  base: '/sonachala-userfrontend/', // must match your repo name
 })
