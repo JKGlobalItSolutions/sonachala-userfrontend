0dd4c4d25d82335227762e8b4384cd0097473693
sriram A <sriram.coder2004@gmail.com>
2025-07-08 17:38:46 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import {  Router, Route, Routes } from "react-router-dom";
 
 
 import { AuthProvider } from "./contexts/AuthContext";
